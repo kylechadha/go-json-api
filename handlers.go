1f51753f8cb2ef952d8840eb495abd5469b1f70b
Kyle Chadha <kyle.chadha@gmail.com>
2015-02-05 21:49:03 -0500

    Add a fake db and add some more routes

--- handlers.go
+++ handlers.go
@@ -13,11 +13,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Master the universe"},
-	}
-
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOk)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
@@ -27,4 +24,36 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	todoId := vars["todoId"]
 	fmt.Fprintln(w, "Todo Show:", todoId)
+}
+
+func TodoCreate(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+
+	// First we open up the body of the request
+	// *and* we make sure to use io.LimitReader to limit the size of the request we'll parse (for mailicious attacks)
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	// Then we unmarshal it to our Todo struct
+	if err := json.Unmarshal(body, &todo); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		// If unmarshalling fails, we'll send back a 422 and the error in a JSON string
+		w.WriteHeader(422) // unprocessable entitty
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	// If all goes well, we send a 201 (successfully created)
+	t := RepoCreateTodo(todo)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
 }
