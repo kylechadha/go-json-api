79125c7cec4997041e2c23f31f389966706180ce
Kyle Chadha <kyle.chadha@gmail.com>
2015-02-05 20:05:34 -0500

    Integrate negroni

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"os"
+
+	"github.com/codegangsta/negroni"
 )
 
 func main() {
 
+	// CONFIG
+	// --------------
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+	n := negroni.New(
+		negroni.NewRecovery(),
+		// negroni.HandlerFunc(AppMiddleware),
+		negroni.NewLogger(),
+	)
+	n.UseHandler(router)
+
+	// SERVER
+	// --------------
+	n.Run(":" + port)
 
 }
