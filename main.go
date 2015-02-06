f03dd1976c87e784a8b87d2622bcec25b65eae50
Kyle Chadha <kyle.chadha@gmail.com>
2015-02-05 19:28:10 -0500

    Split out handlers and routes into explicit files

--- main.go
+++ main.go
@@ -1,39 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-	router.HandleFunc("/todos/{todoId}", TodoShow)
-
+	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Master the universe"},
-	}
-
-	json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo Show:", todoId)
 }
